b1d92e7e57ec9b3b76cbdd98c5a8ebb211d42848
CatLuck2 <freeaddress834@gmail.com>
2022-04-30 11:48:24 +0900

    コメントを追加

--- NekokichiKadai6/ViewController.swift
+++ NekokichiKadai6/ViewController.swift
@@ -4,24 +4,32 @@
 //
 //  Created by Nekokichi on 2022/04/30.
 //
+/*
+ 工夫したこと
+ ・リテラルを減らし、変数やプロパティで代用
+    ・箇所：UILabel,UISlider,UIAlertController
+    ・理由：リテラルの書き換えをコンパイルで検出できないから
+ ・あたりとはずれの場合をif文で網羅
+ */
 
 import UIKit
 
 final class ViewController: UIViewController {
 
     @IBOutlet weak private var slider: UISlider!
-
     @IBOutlet weak private var answerNumberLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // 初期設定
         answerNumberLabel.text = String(arc4random_uniform(100))
         slider.setUp()
         slider.reset()
     }
 
     @IBAction func checkAnswerButton(_ sender: UIButton) {
+        // extensionのプロパティを使用するために定義
         var alert = UIAlertController()
         var alertMessage: String {
             if Int(answerNumberLabel.text ?? "") ?? 0 == Int(slider.value) {
@@ -39,6 +47,7 @@ final class ViewController: UIViewController {
     }
 }
 
+// スライダーのリテラルを防ぐため
 private extension UISlider {
     func setUp() {
         self.maximumValue = 100.0
@@ -50,6 +59,7 @@ private extension UISlider {
     }
 }
 
+// 文字列のリテラルを防ぐため
 private extension UIAlertController {
     var alertTitle: String {
         "結果"
