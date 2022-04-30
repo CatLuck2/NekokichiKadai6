7a89c9a54035d67c3dcdaf803fbe37154789060e
CatLuck2 <freeaddress834@gmail.com>
2022-04-30 09:28:32 +0900

    プロジェクトを新規作成

--- NekokichiKadai6/ViewController.swift
+++ NekokichiKadai6/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var answerNumberLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func slider(_ sender: UISlider) {
+    }
+
+    @IBAction func checkAnswerButton(_ sender: UIButton) {
+    }
 
 }
-
