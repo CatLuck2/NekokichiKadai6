d42afc14ced33564ae6606bb7c15df7633fdcce7
CatLuck2 <freeaddress834@gmail.com>
2022-04-30 10:16:30 +0900

    必要な機能を全て実装

--- NekokichiKadai6/ViewController.swift
+++ NekokichiKadai6/ViewController.swift
@@ -9,17 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
+
     @IBOutlet weak var answerNumberLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
-    @IBAction func slider(_ sender: UISlider) {
+        answerNumberLabel.text = "\(arc4random_uniform(100))"
+        slider.maximumValue = 100.0
+        slider.minimumValue = 0.0
+        slider.value = 50.0
     }
 
     @IBAction func checkAnswerButton(_ sender: UIButton) {
+        var alertMessage: String {
+            if Int(answerNumberLabel.text ?? "") ?? 0 == Int(slider.value) {
+                return "あたり！\nあなたの値：\(Int(slider.value))"
+            } else {
+                return "はずれ！\nあなたの値：\(Int(slider.value))"
+            }
+        }
+        let alert: UIAlertController = UIAlertController(title: "結果", message: alertMessage, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "再挑戦", style: .cancel, handler: { _ in
+            self.answerNumberLabel.text = "\(arc4random_uniform(100))"
+            self.slider.value = 50.0
+        }))
+        present(alert, animated: true, completion: nil)
     }
 
 }
