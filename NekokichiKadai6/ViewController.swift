4558fbbfe9d966710dc55354e025cf6ce0821e21
CatLuck2 <freeaddress834@gmail.com>
2022-05-10 17:24:05 +0900

    必要な機能を実装

--- NekokichiKadai6/ViewController.swift
+++ NekokichiKadai6/ViewController.swift
@@ -11,7 +11,28 @@ final class ViewController: UIViewController {
 
     @IBOutlet weak private var slider: UISlider!
     @IBOutlet weak private var answerNumberLabel: UILabel!
+    private var answerNumber = Int.random(in: 1...100)
 
     @IBAction func checkAnswerButton(_ sender: UIButton) {
+        let sliderValue = Int(slider.value)
+
+        if sliderValue == answerNumber {
+            showAlert(message: "あたり!\nあなたの値:\(sliderValue)")
+        } else {
+            showAlert(message: "はずれ!\nあなたの値:\(sliderValue)")
+        }
+    }
+
+    override func viewDidLoad() {
+        answerNumberLabel.text = "\(answerNumber)"
+    }
+
+    private func showAlert(message: String) {
+        let alertController = UIAlertController(title: "結果", message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .cancel, handler: { _ in
+            self.answerNumber = Int.random(in: 1...100)
+            self.answerNumberLabel.text = "\(self.answerNumber)"
+        }))
+        present(alertController, animated: true, completion: nil)
     }
 }
